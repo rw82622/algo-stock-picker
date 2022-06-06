9d3e6b2962c4acc71d0562c9050a6a10d22b9cd4
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-06 13:46:08 -0500

    solve stock picker challenge in python

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,13 @@
 def picker(prices):
-    pass
+    buy_index = 0
+    gains = 0
+    sell_index = 0
+    
+    for i in range(1, len(prices)):
+        if prices[i] < prices[buy_index]:
+            buy_index = i
+        elif prices[i] - prices[buy_index] > gains:
+            gains = prices[i] - prices[buy_index]
+            sell_index = i
+    buy_index = prices.index(min(prices[0:sell_index]))
+    return [buy_index, sell_index]
