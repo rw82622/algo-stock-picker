d66ce946ca5458ac402c6305acabc5f5a04fbe5e
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-06 14:09:48 -0500

    solve stock picker challenge in js, install shallow equal

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,16 @@
-exports.picker = function(prices) {
+exports.picker = function (prices) {
+  let buy_index = 0;
+  let gains = 0;
+  let sell_index = 0;
 
-}
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < prices[buy_index]) {
+      buy_index = i;
+    } else if (prices[i] - prices[buy_index] > gains) {
+      gains = prices[i] - prices[buy_index];
+      sell_index = i;
+    }
+  }
+  buy_index = prices.indexOf(Math.min(...prices.slice(0, sell_index)));
+  return [buy_index, sell_index];
+};
